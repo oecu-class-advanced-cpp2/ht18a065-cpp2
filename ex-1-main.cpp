6ae76353985443b4ee06c891ba161c9b0b5c7bfa
ht18a065 <ht18a065@oecu.jp>
2020-11-10 13:00:33 +0900

    プログラムの完成

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -16,11 +16,36 @@
 /* -------------------------------------------------------------- */
 int nth_prime(unsigned int a, unsigned int d, unsigned int n)
 {
+	int x, y, yakusuu;
+	int times = 0;
+	for (x = a; x <= 1000000; x += d)
+	{
+		yakusuu = 0;
+		for (y = 1; y <= x; y++) {
+			if (x%y == 0)
+				yakusuu++;
+		}
+		if (yakusuu == 2)
+			times += 1;
+			if (times == n)
+				return x;
+	}
 }
 
 int main()
 {
-	std::cout << nth_prime(367, 186, 151) << std::endl;
+	std::cout <<"要素数・ 367, 186, 151"<<"   "<<"正・92809 "<< "　解・"<<nth_prime(367, 186, 151) << std::endl;
+	std::cout <<"要素数・ 179,  10, 203"<<"   "<<"正・6709  "<< "　解・" <<nth_prime(179, 10, 203) << std::endl;
+	std::cout <<"要素数・ 271,  37,  39"<<"   "<<"正・12037 "<< "　解・" <<nth_prime(271, 37, 39) << std::endl;
+	std::cout <<"要素数・ 103, 230,   1"<<"   "<<"正・103   "<< "　解・" <<nth_prime(103, 230, 1) << std::endl;
+	std::cout <<"要素数・  27, 104, 185"<<"   "<<"正・93523 "<< "　解・" <<nth_prime(27, 104, 185) << std::endl;
+	std::cout <<"要素数・ 253,  50,  85"<<"   "<<"正・14503 "<< "　解・" <<nth_prime(253, 50, 85) << std::endl;
+	std::cout <<"要素数・   1,   1,   1"<<"   "<<"正・2     "<< "　解・" <<nth_prime(1, 1, 1) << std::endl;
+	std::cout <<"要素数・9075, 337, 210"<<"   "<<"正・899429"<< "　解・" <<nth_prime(9075, 337, 210) << std::endl;
+	std::cout <<"要素数・ 307,  24,  79"<<"   "<<"正・5107  "<< "　解・" <<nth_prime(307, 24, 79) << std::endl;
+	std::cout <<"要素数・ 331, 221, 177"<<"   "<<"正・412717"<< "　解・" <<nth_prime(331, 221, 177) << std::endl;
+	std::cout <<"要素数・ 259, 170,  40"<<"   "<<"正・22699 "<< "　解・" <<nth_prime(259, 170, 40) << std::endl;
+	std::cout <<"要素数・ 269,  58, 102"<<"   "<<"正・25673 "<< "　解・" <<nth_prime(269, 58, 102) << std::endl;
 	//以下、同様に、入出力例通りになるか確認せよ。
 	
     std::cin.get();
